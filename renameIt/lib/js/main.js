639cae9c0fe43243c397e742e38ab2226d28b687
SaeedASC22 <sbashiru@stu.allstarcode.org>
2022-08-02 11:16:38 -0400

    Update main.js

---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,6 @@
+let heading = document.getElementById('item_name');
+
+heading.innerHTML = "Frostfang";
+
+let image = document.getElementById("item_img");
+image.src =  "assets/Frostfang_item.png";
